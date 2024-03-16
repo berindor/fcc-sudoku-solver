219a0a18a13948926a764cff38b6c3be8eb1a771
Beringer Dorottya <beringerdorottya@gmail.com>
2024-03-16 12:44:41 +0100

    added solver.validate and tests added test for solver.check functions

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,17 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    if (puzzleString.length !== 81) return 'Expected puzzle to be 81 characters long';
+    if (!/^[1-9\.]{81}$/.test(puzzleString)) return 'Invalid characters in puzzle';
+    return true;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,47 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
+
+const { puzzlesAndSolutions: puzzleStrings } = require('../controllers/puzzle-strings');
 
 suite('Unit Tests', () => {
-
+  const validPuzzle = puzzleStrings[0][0];
+  suite('Validate input', () => {
+    test('input puzzle is 81 characters long with valid characters => true', () => {
+      assert.isOk(solver.validate(validPuzzle));
+    });
+    test('input puzzle consists only valid characters 1-9 and . => "Invalid characters in puzzle"', () => {
+      const invalidPuzzle = puzzleStrings[0][0].slice(5) + 'ab/..';
+      assert.equal(solver.validate(invalidPuzzle), 'Invalid characters in puzzle');
+    });
+    test('input puzzle is not 81 characters long => "Expected puzzle to be 81 characters long"', () => {
+      const shortPuzzle = puzzleStrings[0][0].slice(5);
+      assert.equal(solver.validate(shortPuzzle), 'Expected puzzle to be 81 characters long');
+    });
+  });
+  suite('Check row', () => {
+    test('no conflict in row => true', () => {
+      assert.isOk(solver.checkRowPlacement(validPuzzle, 'A', 2, 3));
+    });
+    test('conflict in row => false', () => {
+      assert.isNotOk(solver.checkRowPlacement(validPuzzle, 'A', 2, 8));
+    });
+  });
+  suite('Check column', () => {
+    test('no conflict in column => true', () => {
+      assert.isOk(solver.checkColPlacement(validPuzzle, 'A', 2, 3));
+    });
+    test('conflict in column => false', () => {
+      assert.isNotOk(solver.checkColPlacement(validPuzzle, 'A', 2, 7));
+    });
+  });
+  suite('Check region', () => {
+    test('no conflict in region => true', () => {
+      assert.isOk(solver.checkRegionPlacement(validPuzzle, 'A', 2, 3));
+    });
+    test('conflict in region => false', () => {
+      assert.isNotOk(solver.checkRegionPlacement(validPuzzle, 'A', 2, 6));
+    });
+  });
 });
